c5b8135078bc4f990b3879be4749554b13192322
Your Name <you@example.com>
2024-02-27 18:38:59 +0700

    pembaruan

---
+++ Tugas_3/script.js
@@ -0,0 +1,7 @@
+
+function test() {
+    var nama = document.getElementById('exampleInputName1').value;
+    var jum = document.getElementById('exampleInputJumlah1').value;
+    
+    alert('nama anda '+nama+' jumlah nya : '+jum);
+}
