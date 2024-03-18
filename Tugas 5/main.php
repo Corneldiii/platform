cd1e601083dfa0f24b6db7dd250ef10f1845a7e3
Your Name <you@example.com>
2024-03-18 19:42:12 +0700

    penambahan

--- Tugas 5/main.php
+++ Tugas 5/main.php
@@ -1,5 +1,6 @@
 <?php
 include "Server/serverDB.php";
+
 session_start();
 if (isset($_SESSION['user_id'])) {
     $ID = $_SESSION['user_id'];
@@ -9,11 +10,21 @@ if (isset($_SESSION['user_id'])) {
 if (isset($_POST['submit'])) {
     $textlist = $_POST['textList'];
 
-    $sql = "INSERT INTO daftarlist (list,status,id_user) VALUES
+    if ($textlist != "") {
+        $sql = "INSERT INTO daftarlist (list,status,id_user) VALUES
         ('$textlist','simpan',$ID)";
 
-    $result = $db->Query($sql);
+        $result = $db->Query($sql);
+    } else {
+?>
+        <script>
+            alert("Isi dulu dong");
+        </script>
+<?php
+    }
 }
+
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -23,24 +34,43 @@ if (isset($_POST['submit'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style.css">
+    
 
     <title>Hello, world!</title>
 </head>
 
 <body>
-
     <section id='todo-page'>
-        <div id="listpg">
-            <h1 class="text-center">To Do List</h1>
-            <form action="main.php" method="post">
-                <div class="input-group mb-3 mt-5">
-                    <input type="text" class="form-control" placeholder="Masukan To Do List" name="textList">
-                    <button type="submit" class="btn btn-primary ml-2" name="submit">Submit</button>
-                </div>
-            </form>
-        </div>
+        <div id="page">
+            <div id="listpg">
+                <h1 class="text-center">To Do List</h1>
+                <form action="main.php" method="post">
+                    <div class="input-group mb-3 mt-5">
+                        <input type="text" class="form-control" placeholder="Masukan To Do List" name="textList">
+                        <button type="submit" class="btn btn-primary ml-2" name="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
 
+            <div class="listdaftar">
+                <?php
+                if (isset($ID)) {
+                    $sql = "select list,status from daftarlist where id_user = '$ID'";
+
+                    $result = $db->Query($sql);
+
+                    if ($result->num_rows > 0) {
+                        for ($i=0; $i < 7; $i++) { 
+                            ?>
+                                <div id="daftar">cek</div>
+                            <?php
+                        }
+                    }
+                }
+                ?>
+            </div>
+        </div>
     </section>
 
 
